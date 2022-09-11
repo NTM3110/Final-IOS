7c037a585a9d37bb9d248d823e3a1b89d9f8f7fb
NTM3110 <mdmt3110@gmail.com>
2022-09-11 16:11:23 +0700

    change endpoints

--- src/main/java/rmit/rmitsb/controller/ArticleController.java
+++ src/main/java/rmit/rmitsb/controller/ArticleController.java
@@ -13,27 +13,27 @@ public class ArticleController {
     @Autowired
     private ArticleService articleService;
 
-    @RequestMapping(path = "/students", method = RequestMethod.GET)
+    @RequestMapping(path = "/articles", method = RequestMethod.GET)
     public List<ArticleModel> getAllArticles(){
         return articleService.getAllArticles();
     }
 
-    @RequestMapping(path = "/students", method = RequestMethod.POST)
+    @RequestMapping(path = "/articles", method = RequestMethod.POST)
     public void addArticle(@RequestBody ArticleModel  article){
          articleService.saveArticle(article);
     }
 
-    @RequestMapping(path = "/students", method = RequestMethod.PUT)
+    @RequestMapping(path = "/articles", method = RequestMethod.PUT)
     public void updateArticle(@RequestBody ArticleModel article){
         articleService.saveArticle(article);
     }
 
-    @RequestMapping(path = "/students/{id}", method = RequestMethod.DELETE)
+    @RequestMapping(path = "/articles/{id}", method = RequestMethod.DELETE)
     public void deleteArticle(@PathVariable(value = "id") Long id){
         articleService.deleteArticle(id);
     }
 
-    @RequestMapping(path = "/students/{id}", method = RequestMethod.GET)
+    @RequestMapping(path = "/articles/{id}", method = RequestMethod.GET)
     public ArticleModel getArticle(@PathVariable(value = "id") Long id){
         return articleService.getArticle(id);
     }
